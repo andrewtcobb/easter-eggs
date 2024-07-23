c03b074a3b90690ea44207b733fc482d5cf3937d
Drew Cobb <andrewtcobb94@gmail.com>
2024-07-23 08:31:23 -0400

    remove audio on click for flying image

--- lib/components/flying-image/index.tsx
+++ lib/components/flying-image/index.tsx
@@ -1,14 +1,9 @@
 import beachBall from "../../assets/beach_ball.png";
-import helloThere from "../../assets/hello_there.mp3";
 import { useEasterEgg } from "../../hooks/useEasterEgg";
 import styles from "./styles.module.css";
 
 export function FlyingImage({ image = beachBall }) {
   const isVisible = useEasterEgg();
-
-  const playAudio = () => {
-    new Audio(helloThere).play();
-  };
 
   return (
     <>
@@ -19,7 +14,6 @@ export function FlyingImage({ image = beachBall }) {
               id="flying-image"
               src={image}
               alt="flying-image"
-              onClick={playAudio}
               style={{ cursor: "pointer" }}
             />
           </>
